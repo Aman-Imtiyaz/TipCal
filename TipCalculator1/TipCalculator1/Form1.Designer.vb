c634b25fceeeacf6ed31504dfb079cdf2dd62dc3
Aman-Imtiyaz <84123867+Aman-Imtiyaz@users.noreply.github.com>
2021-05-13 19:28:22 +0530

    Add files via upload

---
+++ TipCalculator1/TipCalculator1/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtBillAmount = New System.Windows.Forms.TextBox()
+        Me.txtTipPercent = New System.Windows.Forms.TextBox()
+        Me.txtNoOfPerson = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTipPerPerson = New System.Windows.Forms.TextBox()
+        Me.txtTotalPerPerson = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtNoOfPerson)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtTipPercent)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtBillAmount)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Button1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtTotalPerPerson)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtTipPerPerson)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label5)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label4)
+        Me.SplitContainer1.Size = New System.Drawing.Size(718, 290)
+        Me.SplitContainer1.SplitterDistance = 239
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Bill Amount"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Tip Percent"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 186)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 21)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "No of Person"
+        '
+        'txtBillAmount
+        '
+        Me.txtBillAmount.Location = New System.Drawing.Point(12, 52)
+        Me.txtBillAmount.Name = "txtBillAmount"
+        Me.txtBillAmount.Size = New System.Drawing.Size(138, 29)
+        Me.txtBillAmount.TabIndex = 3
+        '
+        'txtTipPercent
+        '
+        Me.txtTipPercent.Location = New System.Drawing.Point(12, 135)
+        Me.txtTipPercent.Name = "txtTipPercent"
+        Me.txtTipPercent.Size = New System.Drawing.Size(138, 29)
+        Me.txtTipPercent.TabIndex = 4
+        '
+        'txtNoOfPerson
+        '
+        Me.txtNoOfPerson.Location = New System.Drawing.Point(12, 224)
+        Me.txtNoOfPerson.Name = "txtNoOfPerson"
+        Me.txtNoOfPerson.Size = New System.Drawing.Size(138, 29)
+        Me.txtNoOfPerson.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(54, 84)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(108, 21)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Tip Per Person"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(54, 186)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(119, 21)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Total Per Person"
+        '
+        'txtTipPerPerson
+        '
+        Me.txtTipPerPerson.Location = New System.Drawing.Point(200, 81)
+        Me.txtTipPerPerson.Name = "txtTipPerPerson"
+        Me.txtTipPerPerson.Size = New System.Drawing.Size(150, 23)
+        Me.txtTipPerPerson.TabIndex = 2
+        '
+        'txtTotalPerPerson
+        '
+        Me.txtTotalPerPerson.Location = New System.Drawing.Point(199, 183)
+        Me.txtTotalPerPerson.Name = "txtTotalPerPerson"
+        Me.txtTotalPerPerson.Size = New System.Drawing.Size(151, 23)
+        Me.txtTotalPerPerson.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(345, 135)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(718, 290)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents txtNoOfPerson As TextBox
+    Friend WithEvents txtTipPercent As TextBox
+    Friend WithEvents txtBillAmount As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtTotalPerPerson As TextBox
+    Friend WithEvents txtTipPerPerson As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ TipCalculator1/TipCalculator1/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim amt, tippercent, noofperson, tipperperson, totalperperson As Double
+        amt = txtBillAmount.Text
+        tippercent = txtTipPercent.Text
+        noofperson = txtNoOfPerson.Text
+        tipperperson = (txtBillAmount.Text * txtTipPercent.Text) / (100 * txtNoOfPerson.Text)
+        totalperperson = (txtBillAmount.Text / txtNoOfPerson.Text) + tipperperson
+        txtTipPerPerson.Text = tipperperson
+        txtTotalPerPerson.Text = totalperperson
+
+
+    End Sub
+End Class
